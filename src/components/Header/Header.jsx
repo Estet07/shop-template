d3389c9b9a7b4edc7454ffa9c308fe88bd82e8e8
estet07 <estetaleksandr@gmail.com>
2023-04-28 00:01:07 +0600

    addNavigate

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,37 +1,51 @@
-import React from 'react';
-import styles from './header.module.css'
-import Modal from '../Modal/Modal';
+import React from "react";
+import styles from "./header.module.css";
+import Modal from "../Modal/Modal";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-        <header>
-            <div className={styles.wrapper} >
-                <nav className={styles.nav}>
-                    <a href="/" className={styles.link}>Home</a>
-                    <a href="/" className={styles.link}>Shop</a>
-                    <a href="/" className={styles.link}>About</a>
-                    <a href="/" className={styles.link}>Contact</a>
-                </nav>
-                <div className={styles.icons}>
-                    <a href="/" className={styles.icon}>
-                        <img src="/images/profile-icon.png" alt="" className={styles.icon} />
-                    </a>
-                    <p className={styles.icon}>
-                        <img src="/images/search-icon.png" alt="" className={styles.icon} />
-                    </p>
-                    <p className={styles.icon}>
-                        <img src="/images/favorite-icon.png" alt="" className={styles.icon} />
-                    </p>
-                    <p className={styles.icon}>
-                        <span className={styles.quantity}>1</span>
-                        <img src="/images/basket-icon.png " alt="" className={styles.icon} />
-                    </p>
-                </div>
-            </div>
-        </header>
-        </>
-    );
+  const activeClass = ({ isActive }) => {
+    return isActive ? `${styles.active} ${styles.link}` : styles.link;
+  };
+  return (
+    <>
+      <header>
+        <div className={styles.wrapper}>
+          <nav className={styles.nav}>
+            <NavLink to="/" className={activeClass}>Home</NavLink>
+            <NavLink to="/account" className={activeClass}>Account</NavLink>
+            <NavLink to="/catalog" className={activeClass}>Shop</NavLink>
+          </nav>
+          <div className={styles.icons}>
+            <NavLink to="/contacts" className={styles.icon}>
+              <img src="/images/profile-icon.png" alt="" className={styles.icon}/>
+            </NavLink>
+            {/* <a href="/" className={styles.icon}>
+              <img src="/images/profile-icon.png" alt="" className={styles.icon}/>
+            </a> */}
+            <p className={styles.icon}>
+              <img src="/images/search-icon.png" alt="" className={styles.icon}/>
+            </p>
+            <p className={styles.icon}>
+              <img src="/images/favorite-icon.png" alt="" className={styles.icon}/>
+            </p>
+            <NavLink to="/cart" className={styles.icon}>
+            <span className={styles.quantity}>1</span>
+              <img src="/images/basket-icon.png" alt="" className={styles.icon}/>
+            </NavLink>
+            {/* <p className={styles.icon}>
+              <span className={styles.quantity}>1</span>
+              <img
+                src="/images/basket-icon.png "
+                alt=""
+                className={styles.icon}
+              />
+            </p> */}
+          </div>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
