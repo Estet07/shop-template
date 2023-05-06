b871bd1b68b0ef98762dd4104074b0b81b2f6b1a
estet07 <estetaleksandr@gmail.com>
2023-05-06 22:28:03 +0600

    addModalCart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./header.module.css";
 import Modal from "../Modal/Modal";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const cartQuantity = useSelector(state => state.cart.quantityCart)
   const activeClass = ({ isActive }) => {
     return isActive ? `${styles.active} ${styles.link}` : styles.link;
   };
@@ -28,7 +30,7 @@ const Header = () => {
               <img src="/images/favorite-icon.png" alt="" className={styles.icon}/>
             </p>
             <NavLink to="/cart" className={styles.icon}>
-            <span className={styles.quantity}>1</span>
+            {cartQuantity > 0 &&<span className={styles.quantity}>{cartQuantity}</span>}
               <img src="/images/basket-icon.png" alt="" className={styles.icon}/>
             </NavLink>
             {/* <p className={styles.icon}>
@@ -40,6 +42,7 @@ const Header = () => {
               />
             </p> */}
           </div>
+          { cartQuantity > 0 && <Modal /> }
         </div>
       </header>
     </>
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./modal.module.css";
 
 const Modal = () => {
-  const isOpen = false
+  const products = useSelector(state => state.cart.productsCart)
+  const totalPrice = products.reduce((acc, curr) => acc + curr.price, 0);
+
   return (
-    isOpen && (
-        <div className={styles.wrapper}>
-      <div className={styles.content}>
+    <div className={styles.wrapper}>
+      {products.map(product => {
+        console.log(product);
+        return (
+          <div className={styles.content}>
         <img
           src="/images/basket-close-icon.png"
           alt=""
@@ -16,26 +21,28 @@ const Modal = () => {
         <div className={styles.line}></div>
         <div className={styles.products}>
           <div className={styles.product}>
-            <img src="/images/test-product-img.png" alt="" />
+            <img src={product.img} alt="" />
             <div className={styles.info}>
-              <h5 className={styles.name}>Asgaard Sofa</h5>
-              <p>1 x Rs. 250,000.000</p>
+              <h5 className={styles.name}>{product.title}</h5>
+              <p>{product.price}</p>
             </div>
             <img src="/images/close-icon.png" alt="" />
           </div>
         </div>
         <div className={styles.bottom}>
         <div className={styles.total}>
-            <p className={styles.subtotal}>Subtotal</p> Rs. 250,000.00
+            <p className={styles.subtotal}>Subtotal</p> Rs. {totalPrice}
         </div>
         <div className={styles.line}></div>
             <a href="/cart" className={styles.link}>View Cart</a>
             <a href="/checkout" className={styles.link}>Checkout</a>
         </div>
       </div>
+        );
+      })}
+      
     </div>
     )
-  );
 };
 
 export default Modal;
